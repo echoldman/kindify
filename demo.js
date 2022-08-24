10237f13f3d3d288efa379d35e05da67905088fa
db <db@office>
2022-08-24 18:43:45 +0800

    Create.

---
+++ demo.js
@@ -0,0 +1,61 @@
+const sub_definition = {
+  name: 'Sub',
+  properties: [
+    {
+      name: 'title',
+      type: 'string',
+      nullable: false,
+      default: null,
+    },
+    {
+      name: 'len',
+      type: 'integer',
+      nullable: false,
+      default: null
+    }
+  ],
+  methods: [
+    {
+      name: 'show',
+      params: [],
+      result: {
+        type: 'void'
+      },
+      fun: function () {
+        console.log(`${this.title} ${this.len}`);
+      }
+    },
+    {
+      name: 'setLen',
+      params: [
+        {
+          name: 'len',
+          type: 'integer',
+          nullable: false,
+          default: 20
+        }
+      ],
+      result: {
+        type: 'integer'
+      },
+      fun: function (len) {
+        this.len = len;
+        return len;
+      }
+    }
+  ],
+}
+
+import { define } from './index.js';
+
+const kindSub = define(sub_definition);
+const sub1 = kindSub.create({ title: 'hello, world 1', len: 10 });
+console.log(sub1);
+
+sub1.show();
+
+sub1.setLen();
+sub1.show();
+
+sub1.setLen(24);
+sub1.show();
